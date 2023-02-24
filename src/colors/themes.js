e450297fbe4b26b784e9b198dd8f39b243ecc811
Anis conceptlz <anis@conceptualize.ae>
2023-02-24 16:35:14 +0530

    changed primary color

--- src/colors/themes.js
+++ src/colors/themes.js
@@ -1,9 +1,9 @@
 module.exports = {
     "[data-theme=light]": {
       "color-scheme": "light",
-      primary: "#570df8",
+      primary: "#B68A35",
       "primary-content": "#ffffff",
-      secondary: "#f000b8",
+      secondary: "#171A53",
       "secondary-content": "#ffffff",
       "base-100": "#345da7",
     },
